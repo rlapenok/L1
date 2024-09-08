440cc7650f0c34bad9d47f15b182051a58e987fd
rlapenok <rlapenok@gmail.com>
2024-09-08 20:58:49 +0300

    L1.1

---
+++ task_1/src/main.rs
@@ -0,0 +1,23 @@
+trait Action {
+
+    fn say(&self);
+    
+}
+
+struct Person<'a>{
+    name :&'a str
+}
+
+impl<'a> Action for Person<'a>{
+    fn say(&self) {
+        println!("Hello,{}",self.name);
+    }
+}
+
+
+fn main() {
+    let person=Person{
+        name:"Foo"
+    };
+    person.say();
+}
